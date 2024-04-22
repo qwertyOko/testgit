00b7b520c7fcb8a1a7298f5fadf3f70a9169f82e
qwertyOko <Koval_wl91@student.itstep.org>
2024-04-22 11:19:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Hello, World!");
