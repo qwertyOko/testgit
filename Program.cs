d60e1ae0078617a019ff154b61fc9a16188bdb9b
qwertyOko <Koval_wl91@student.itstep.org>
2024-04-22 11:34:35 +0300

    comit 5
     added color for name/firstname

--- Program.cs
+++ Program.cs
@@ -8,4 +8,12 @@ Console.ForegroundColor = ConsoleColor.Green;
 Console.Write(fullName.Split(' ')[0]);
 Console.ResetColor();
 
+Console.Write(" ");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write(fullName.Split(' ')[1]);
+Console.ResetColor();
+
+Console.Write(" ");
+
 
