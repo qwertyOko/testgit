e7cb33d978dfe7a13400918fb04703faeb7a3858
qwertyOko <Koval_wl91@student.itstep.org>
2024-04-22 11:32:29 +0300

    comit 2
    added input name/firstame

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.BackgroundColor = ConsoleColor.DarkMagenta;
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.WriteLine("Введите ваше ФИО:");
+string fullName = Console.ReadLine();
+
+ 
